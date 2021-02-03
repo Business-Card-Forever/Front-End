19e184eb552381b79277ada16fbaa02f6d47a2c6
Dana Kiswani <danakiswani0@gmail.com>
2021-02-03 13:56:46 +0200

    main bg and registeration

---
+++ src/components/main/main.js
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import './main.css';
+
+import { PageHeader } from "react-bootstrap";
+import Video from "../../img/bg2.mp4";
+
+class VedioBG extends React.Component {
+    render() {
+      return (
+        <div>
+          <video src={Video}   controls="controls" autoplay="true" className="myvideo"/>
+        </div>
+      );
+    }
+  }
+
+export default VedioBG ;
